425e5be5d5bc2ab997310eb40914be5a618b3ce0
Ralph Strenglein <RStrenglein.gmail.com>
2018-11-11 07:30:15 -0500

    Added toString method.

--- src/org/strenglein/model/User.java
+++ src/org/strenglein/model/User.java
@@ -7,7 +7,11 @@ public class User {
 	public User(String name) {
 		this.name = name;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + "]";
+	}
 
 	public String getName() {
 		return name;
