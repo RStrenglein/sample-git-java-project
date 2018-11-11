f9934f41c4ee0b99833831da535dc3db0649473a
Ralph Strenglein <RStrenglein.gmail.com>
2018-11-10 22:19:46 -0500

    Added getter, setter, and constructor.

--- src/org/strenglein/model/User.java
+++ src/org/strenglein/model/User.java
@@ -1,7 +1,22 @@
 package org.strenglein.model;
 
 public class User {
-	
+
 	String name;
+	
+	public User(String name) {
+		this.name = name;
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
